3946b28fa74de1e3efe84b6d9347e8788cbcc0be
fedhinen <victor.adrian.ruiz.espindola@gmail.com>
2025-12-04 16:43:15 -0600

    https

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // Confiar en todos los proxies (Traefik + Cloudflare)
 
     /**
      * The headers that should be used to detect proxies.
